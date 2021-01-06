2e216a868dcc9374c51dcca62fb1cba10da7e064
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-M6IUDRR>
2021-01-06 11:38:44 -0300

    Meu primeiro

---
+++ paginas/includes/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html lang="pt-bt">
+    <head>
+        <meta charset="UTF-8">
+        <title>Contato</title>
+
+        <!--bootstrap-->
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+
+        <!--plugins-->
+
+        <!--icon-->
+        <link rel="stylesheet" href="plugins/iconmoon/iconmoon.css.css" />
+
+        <!--css-->
+        <link rel="stylesheet" href="assets/css/noticias.css" />
+
+    </head>
+    <body>
+        <header class="container">
+
+            <!--slide-->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/img/img1.1.png" alt="Primeiro Slide">             
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/img1.4.png" alt="Segundo Slide">
+                    </div>
+                    <qwdiv class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/img3.jpg" alt="Terceiro Slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="assets/img/img4.jpg" alt="Terceiro Slide">
+                        </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                </a>
+            </div>
+            <!--fim dos slides-->
+
+            <!--barra de navegação-->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#"><spam class="vini-home"></spam></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active"><a class="nav-link" href="?pg=inicio">Inicio </a></li> 
+                        <li class="nav-item active"><a class="nav-link" href="?pg=sobre">Sobre </a></li> 
+                        <li class="nav-item active"><a class="nav-link" href="?pg=servicos">Serviços </a></li> 
+                        <li class="nav-item active"><a class="nav-link" href="?pg=produtos">Produtos </a></li> 
+                        <li class="nav-item active"><a class="nav-link" href="?pg=contato">Contatos </a></li> 
+                    </ul>
+
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                        
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   Área administrativa
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="?pg=login"><span class="vini-user"></span> Login </a>
+                                    <a class="dropdown-item" href="?pg=login2"><span class="vini-user"></span> Login 2 </a>
+                                    <a class="dropdown-item" href="?pg=login3"><span class="vini-user"></span> Login 3 </a>
+                                    <a class="dropdown-item" href="?pg=login4"><span class="vini-user"></span> Login 4 </a>                          
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item alert-danger" href="?pg=login4"><span class="vini-exit"></span> Sair </a>                          
+                                </div>
+                            </li>
+                        </ul>
+
+                    </form>
+
+
+                </div>              
+            </nav> 
+            <!--fim da barra de navegação-->
+
+        </header>
+        <!--fim do header-->
