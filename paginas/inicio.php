5a41c61bddc91cf98a6f25947afae346ee46e656
PROGR WEB - MANHÃ <PROGR WEB - MANH@MININT-9SOJ8BE>
2021-01-07 11:17:49 -0200

    heyyyyy

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -18,7 +18,7 @@
                     </div>
                 </div>
                 <div class="col">
-                    <h1>Black Clover!</h1>
+                    <h1>Site em Manutenção</h1>
                     <p class="text-justify">Black Clover (ãƒ–ãƒ©ãƒƒã‚¯ã‚¯ãƒ­ãƒ¼ãƒ�ãƒ¼ Burakku KurÅ�bÄ�?) Ã© uma sÃ©rie de mangÃ¡ shonen de fantasia escrita e ilustrada por YÅ«ki Tabata. A sÃ©rie Ã© publicada na revista Weekly ShÅ�nen Jump da editora Shueisha, no JapÃ£o, e licenciada e publicada no Brasil pela editora Panini Group. A histÃ³ria se centra em um jovem chamado Asta, aparentemente nascido sem qualquer poder mÃ¡gico, algo inÃ©dito no mundo em que vive, onde todos sÃ£o capazes de usar magia. Com sua ambiÃ§Ã£o, habilidades recÃ©m-descobertas e amigos, ele pretende ser o Rei Mago. Uma OVA produzida pelo estÃºdio Xebec foi lanÃ§ado em 2 de maio de 2017, enquanto uma adaptaÃ§Ã£o em uma sÃ©rie de anime produzida pelo estÃºdio Pierrot estreou em outubro no mesmo ano. Fora do JapÃ£o,  a sequÃªncia dos episÃ³dios do 51 em diante. Black Clover blaaaaaaaaa blaaaaaaaaaaaaaaaaaaa blaaaaaaaaaaaaaa blaaaaaaaaaaaaaaaaaaaaaaaa (ãƒ–ãƒ©ãƒƒã‚¯ã‚¯ãƒ­ãƒ¼ãƒ�ãƒ¼ Burakku KurÅ�bÄ�?) Ã© uma sÃ©rie de mangÃ¡ shonen de fantasia escrita e ilustrada por YÅ«ki Tabata. A sÃ©rie Ã© publicada na revista Weekly ShÅ�nen Jump da editora Shueisha, no JapÃ£o, e A sÃ©rie Ã© publicada na revista Weekly ShÅ�nen Jump da editora Shueisha, A histÃ³ria se centra em um jovem chamado Asta, aparentemente nascido sem qualquer poder mÃ¡gico, algo inÃ©dito no mundo em que vive, onde todos sÃ£o capazes de usar magia. Com sua ambiÃ§Ã£o, habilidades recÃ©m-descobertas e amigos, ele pretende ser o Rei Mago. Uma OVA produzida pelo estÃºdio Xebec foi lanÃ§ado em 2 de maio de 2017.</p>
                 </div>
 
