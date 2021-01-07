3dea816be2e9a0365cb133f18fde4b7682b99ee4
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-M6IUDRR>
2021-01-07 09:19:37 -0300

    facebook1.1

---
+++ paginas/facebook.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Login Facebook</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="style.css" />
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<body>
+	<div id="header">
+		<div class="logo"><a title="Go to Facebook home">facebook</a></div>
+		<div id="nav"><input class="nav-SignUp" type="button" value="Sign Up" name="Sign Up"/></div>
+	</div>
+	<div id="content-wapper">
+		<form id="facebook-login">
+			<div id="facebook-input">
+				<div id="title"><h2>Facebook Login</h2><hr/></div>
+				<table id="tbl-login" border="0px " align="center">
+					<tbody>
+					<tr>
+						<td><label class="label-text">Email or Phone:</label></td>
+						<td ><input class="input-text" type="text" name="Email or Phone"></td>
+					</tr>
+					<tr>
+						<td><label class="label-text">Password:</label></td>
+						<td ><input class="input-text" type="password" name="Password"></td>
+					</tr>
+					<tr>
+
+						<td></td>
+						<td><input type="checkbox" name="Keep me logged in" value=""/><label>Keep me logged in</label></td>
+					<tr>
+
+						<td></td>
+						<td><input  class="btn-login" type="button"value="Log In"/> or
+							<a class="bold-text" href="#">Sign up for Facebook</a>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><a href="#">Forgot  your password ?</a> </td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</form>
+		<div id="lang">
+			<p>
+				<a href="#">English(US)</a>
+				<a href="#">Tiếng Việt</a>
+				<a href="#">中文(台灣)</a>
+				<a href="#">한국어</a>
+				<a href="#">日本語</a>
+				<a href="#">Français (France)</a>
+				<a href="#">ภาษาไทย</a>
+				<a href="#">Español</a>
+				<a href="#">Português (Brasil)</a>
+				<a href="#">Deutsch</a>
+				<i>&#46;&#46;&#46;</i>
+			</p>
+		</div>
+	</div>
+	<div id="footer">
+		<div id="link-pages">
+			<table id="tbl-menu">
+				<tbody>
+				<tr>
+					<td><a href="">Sign Up</a></td>
+					<td><a href="">Log In</a></td>
+					<td><a href="">Mobile</a></td>
+					<td><a href="">Find Friends</a></td>
+					<td><a href="">Badges</a></td>
+					<td><a href="">People</a></td>
+					<td><a href="">Badges</a></td>
+					<td><a href="">Place</a></td>
+					<td><a href="">Games</a></td>
+				</tr>
+				<tr>
+					<td><a href="">Locations</a></td>
+					<td><a href="">About</a></td>
+					<td><a href="">Create Ad</a></td>
+					<td><a href="">Create Page</a></td>
+					<td><a href="">Developers</a></td>
+					<td><a href="">Careers</a></td>
+					<td><a href="">Privacy</a></td>
+					<td><a href="">Cookies</a></td>
+					<td><a href="">Terms</a></td>
+				</tr>
+				<tr>
+					<td><a href="#"> Help</a></td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+		<div id="copyright">
+			<p>Facebook &copy; 2015</p>
+			<p><a href="#"> English(US)</a></p>
+		</div>
+	</div>
+</body>
+<!-- partial -->
+  
+</body>
+</html>
