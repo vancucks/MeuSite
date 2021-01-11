b796266ab75c8b3c1223de6cdd19f94aa8c08a95
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-11 08:15:39 -0300

    djhhfhsfhfvhfvjhfjihegfgdddd

--- paginas/includes/header.php
+++ paginas/includes/header.php
@@ -20,39 +20,7 @@
         <header class="container">
 
             <!--slide-->
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="assets/img/img1.1.png" alt="Primeiro Slide">             
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="assets/img/img1.4.png" alt="Segundo Slide">
-                    </div>
-                    <qwdiv class="carousel-item">
-                        <img class="d-block w-100" src="assets/img/img3.jpg" alt="Terceiro Slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="assets/img/img4.jpg" alt="Terceiro Slide">
-                        </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                </a>
-            </div>
-            <!--fim dos slides-->
-
-            <!--barra de navegação-->
+            
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand" href="#"><spam class="vini-home"></spam></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
